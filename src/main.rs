e81ae14075415830eb90a0b8766846b0c37ff186
Maciej Michalski <legeof008@gmail.com>
2023-10-25 19:13:43 +0200

    feat: add a more advanced socket implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use std::io::{Read, Write};
-use std::os::unix::net::UnixStream;
-
-fn main() -> std::io::Result<()> {
-    let mut unix_socket_stream = UnixStream::connect("/var/run/dispatcher")?;
-    unix_socket_stream.write_all(b"ayee\n")?;
-    let mut data_read_from_socket = String::new();
-    unix_socket_stream.read_to_string(&mut data_read_from_socket)?;
-    println!("{data_read_from_socket}");
-    Ok(())
+use std::io;
+use tokio::io::Interest;
+use tokio::net::UnixStream;
+static SOCKET_ADDRESS: &str = "";
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let stream = UnixStream::connect(SOCKET_ADDRESS).await?;
+    loop {
+        let ready = stream.ready(Interest::READABLE | Interest::WRITABLE).await?;
+        if ready.is_readable() {
+            let mut data = vec![0; 256];
+            match stream.try_read(&mut data) {
+                Ok(n) => {
+                    let read_message: String = data.iter().map(|x| *x as char).collect();
+                    println!("read {} bytes", n);
+                    println!("Message = {}", read_message);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+        }
+    }
 }
