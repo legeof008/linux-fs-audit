8d0056d5932799ef1e9f975e3da9b200c2b2eaf5
Maciej Michalski <legeof008@gmail.com>
2023-11-08 13:51:10 +0100

    add mvc test for http_view.rs

--- src/view/http_view.rs
+++ src/view/http_view.rs
@@ -2,10 +2,6 @@ use crate::serializer::Operation;
 use crate::view::{HttpView, View};
 use async_trait::async_trait;
 use colored::Colorize;
-use reqwest::header::{CONNECTION, CONTENT_TYPE};
-
-const CONNECTION_HEADER_VALUE: &str = "keep-alive";
-const CONTENT_HEADER_VALUE: &str = "application/json";
 
 impl HttpView {
     pub(crate) fn new(destination_url: &str) -> Self {
@@ -20,19 +16,46 @@ impl HttpView {
 impl View for HttpView {
     async fn update(&self, operation: Operation) -> Result<(), ()> {
         let jsonized_operation = serde_json::to_string(&operation).unwrap();
+        log::debug!(
+            "Sending {} to endpoint {}",
+            jsonized_operation.blue(),
+            self.destination_url.to_string().green()
+        );
         let _resp = self
             .client
             .post(self.destination_url.to_string())
             .body(jsonized_operation)
-            .header(CONNECTION, CONNECTION_HEADER_VALUE)
-            .header(CONTENT_TYPE, CONTENT_HEADER_VALUE)
             .send()
             .await;
-        if _resp.is_ok() {
-            log::info!("{}", "Operation has been reported".green())
-        } else {
-            log::error!("{}", "Operation has not been reported".red())
-        }
         return Ok(());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::serializer::Operation;
+    use crate::view::{HttpView, View};
+    const COMPLIANT_LOG_LINE: &str = "type=SYSCALL msg=audit(1698576562.955:570): arch=c000003e syscall=257 success=yes exit=3 a0=ffffff9c a1=55a917750550 a2=90800 a3=0 items=1 ppid=20120 pid=20680 auid=1000 uid=1000 gid=1000 euid=1000 suid=1000 fsuid=1000 egid=1000 sgid=1000 fsgid=1000 tty=pts2 ses=14 comm=\"ls\" exe=\"/usr/bin/ls\" subj=unconfined key=\"READ\"ARCH=x86_64 AUID=\"maciek\" UID=\"maciek\" GID=\"maciek\" EUID=\"maciek\" SUID=\"maciek\" FSUID=\"maciek\" EGID=\"maciek\" SGID=\"maciek\"";
+
+    #[tokio::test]
+    async fn when_sending_operation_msg_server_should_receive_request() {
+        // Mock server setup
+        let mut destination_server = mockito::Server::new();
+        let mut url = destination_server.url();
+        url.push_str("/operations");
+
+        // Creating mock
+        let mock = destination_server
+            .mock("POST", "/operations")
+            .with_status(201)
+            .create();
+
+        // Reporting an operation
+        let http_view = HttpView::new(url.as_str());
+        let response = http_view
+            .update(Operation::new(COMPLIANT_LOG_LINE.to_string()).unwrap())
+            .await;
+        assert!(response.is_ok());
+        mock.assert();
     }
 }
